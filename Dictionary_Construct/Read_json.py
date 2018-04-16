89524d35e925503e424f5aea1f513bf4b2acf25d
yaolinxia <18860976931@163.com>
2018-04-16 21:33:18 +0800

    extract_key

---
+++ Dictionary_Construct/Read_json.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+import json
+
+with open("testJson.json", encoding='utf-8') as f:
+    #将json转化为字典
+    temp = json.loads(f.read())
+    print(temp)
+    len1 = len(temp)
+    i = 0
+    print(len(temp))
+    while i < len1:
+        for case in temp:
+            print(case['name'] +' '+ str(case['count']))
+        i += 1
+
+    #print(temp['id']+' '+temp['name'])
+    #print(temp['name'])
+    #print(temp['rule']['namespace'])
